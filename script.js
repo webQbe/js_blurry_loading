b2d3073a6c3c7f453dc528118a1dc89551741156
webQbe <webcubesolutionslk@gmail.com>
2024-11-22 20:19:51 +0530

    Run blurring() every 30ms to log load's value

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const bg = document.querySelector('.bg');
 // Define Globals
 let load = 0;
 
+// Run blurring() every 30ms
+let int = setInterval(blurring, 30);
+
 function blurring(){
 
     // Increment load value by 1
