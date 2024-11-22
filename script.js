31c986399993de4ab3e30724c500176a479baa38
webQbe <webcubesolutionslk@gmail.com>
2024-11-22 20:18:56 +0530

    Defined blurring()

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // Define DOM Elements
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
+
+// Define Globals
+let load = 0;
+
+function blurring(){
+
+    // Increment load value by 1
+    load++
+
+    // Log load's value
+    console.log(load);
+}
